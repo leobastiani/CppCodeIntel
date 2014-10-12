9959bb807517aaae5582dbd1b0776979a4b11f4f
leobastiani <leogbastiani@gmail.com>
2014-10-12 12:35:58 -0300

    Update CppCodeIntel.py

--- CppCodeIntel.py
+++ CppCodeIntel.py
@@ -4,29 +4,28 @@ import re
 import sublime
 import sublime_plugin
 
-if sys.platform != "win32" or sys.version_info[:2] >= (3, 0):
-    dir_separate = '/' # Linux/Mac
-else:
-    dir_separate = '\\'
-
-syntax_list = {
+syntax_list = { #all allowed syntax
     "C++": True,
     "C": True,
     "Objective-C" : True,
     "Objective-C++": True
 }
-Debug = True
-enabled = True
+Debug = True #if true, see some comments on console
+enabled = True #if true, this plugin will work, i guess... :)
 
 class CppCodeIntelEventListener(sublime_plugin.EventListener):
     def __init__(self):
-        self.completions = [] # aqui vao todos os snippets, é regenerado a todo momento
-        self.files = {} # aqui vao todos os arquivos
-        # self.files['file.c']['word'] vao todas as palavras
+        self.completions = [] # aqui vao todos os snippets, é regenerado a todo momento # all snippets, it will be regenerate everytime
+        self.files = {} # aqui vao todos os arquivos # all files
+        # self.files['file.c']['func'] vao todas as palavras # self.files['file.c']['func'] == self.completions['func']
+        settings = sublime.load_settings("cppcodeintel.sublime-settings")
+        self.show_only_last_word = settings.get("show_only_last_word", False)
 
     def isEnabled(self, view):
         syntax, _ = os.path.splitext(os.path.basename(view.settings().get('syntax')))
         if syntax_list.get(syntax) == None or not enabled:
+            return False
+        elif view.file_name() == None:
             return False
         return syntax_list.get(syntax)
 
@@ -35,12 +34,12 @@ class CppCodeIntelEventListener(sublime_plugin.EventListener):
             return ;
         elif not self.isEnabled(view):
             return ;
-        self.loadFile(view.file_name(), False, self.getContentFromView(view))
+        self.loadFile(view.file_name(), False, self.getContentsFromView(view))
 
     def on_post_save_async(self, view):
         if not self.isEnabled(view):
             return ;
-        self.loadFile(view.file_name(), True, self.getContentFromView(view))
+        self.loadFile(view.file_name(), True, self.getContentsFromView(view))
 
     def on_close(self, view):
         if not self.isEnabled(view):
@@ -56,57 +55,73 @@ class CppCodeIntelEventListener(sublime_plugin.EventListener):
         return self.completions
 
     def removeFile(self, file_path):
+        """
+        this function is called when a file is removed, 
+        this file will be removed from self.files too
+        """
         file_name = os.path.basename(file_path)
         path = os.path.dirname(file_path)
         if self.files.get(file_name) != None:
             print('CppCodeIntel: removing file: '+file_name)
             del self.files[file_name]
-            with open(path+dir_separate+file_name, "r") as file:
+            with open(os.path.join(path, file_name), "r") as file:
                 contents = file.read()
             includes = getIncludesFromContent(contents)
             for include in includes:
-                self.removeFile(path+dir_separate+include)
+                self.removeFile(os.path.join(path, include))
 
-    def loadFile(self, file, override_file=False, contents=None):
-        if contents == None:
+    def loadFile(self, file, override_file=False, file_contents=None):
+        """
+        this function will load a file in self.files
+        """
+        if file_contents == None:
             f = open(file, 'r')
-            contents = f.read()
+            file_contents = f.read()
             f.close()
         file_name = os.path.basename(file)
         dir_name = os.path.dirname(file)
         if not (self.files.get(file_name) == None or override_file):
             return ;
         if Debug: print("CppCodeIntel: loading file '"+file_name+"'")
-        # reseta os snippets
+        # reseta os snippets do arquivo file_name # clear all snippets of file_name
         self.files[file_name] = {}
-        #adicionando funcoes do tipo int func();
-        matches = re.compile('(\w+)\** +(?:\w+ +)*\** *([\w]+) *\(([^\)]*)\)').findall(contents)
-        for match in matches:
-            if match[0] == 'return': # evitar confilitos como a linha 'return func(arg1, arg2);'
+        #adicionando funcoes do tipo int func(parameters) # adding functions like int func(parameters)
+        funcs = getFunctionsFromContent(file_contents)
+        for (type, func_name, parameters) in funcs:
+            if type == 'return': # evitar confilitos como a linha 'return func(arg1, arg2);'
                 continue
-            elif match[1] == 'main': # nao cria snippet para main
+            elif func_name == 'main': # nao adiciona main aos snippets
+                continue
+            elif func_name == 'if': # nao adiciona if aos snippets
                 continue
             count = 1
-            splited = match[2].split(', ')
-            for j, args in enumerate(splited):
-                if args == '':
+            params_splited = parameters.split(', ')
+            for i, arg in enumerate(params_splited):
+                if arg == '':
                     continue
+                elif self.show_only_last_word == False:
+                    snippet_word = arg
                 else:
-                    last_word = re.search('\w+$', args).group()
-                splited[j] = '${'+str(count)+':'+last_word+'}'
+                    try:
+                        snippet_word = re.search('\w+(?:\[(?:\w+)?\])*$', arg).group()
+                    except:
+                        snippet_word = arg
+                params_splited[i] = '${'+str(count)+':'+snippet_word+'}'
                 count += 1
-            self.files[file_name][match[1]] = match[1]+'('+', '.join(splited)+')'
-        #adicionando defines
-        matches = re.compile('\#define +(\w+)').findall(contents)
-        for match in matches:
-            self.files[file_name][match] = match
-        #adicionando includes
-        includes = getIncludesFromContent(contents)
+            self.files[file_name][func_name] = func_name+'('+', '.join(params_splited)+')'
+        important_words = getImportantWordsFromContent(file_contents)
+        for word in important_words:
+            self.files[file_name][word] = word
+        #adicionando includes # adding files in #include "file"
+        includes = getIncludesFromContent(file_contents)
         for include in includes:
-            self.loadFile(dir_name+dir_separate+include)
+            self.loadFile(os.path.join(dir_name, include))
         self.reloadCompletions()
 
     def reloadCompletions(self):
+        """
+        this function makes self.completions
+        """
         if Debug:
             print('CppCodeIntel: reloading completions')
             print('\tfiles to process: '+' '.join(self.files.keys()))
@@ -118,8 +133,32 @@ class CppCodeIntelEventListener(sublime_plugin.EventListener):
                     self.completions += [(func, file[func])]
                     funcs[func] = True
 
-    def getContentFromView(self, view):
+    def getContentsFromView(self, view):
+        """
+        return the contents inside the view
+        """
         return view.substr(sublime.Region(0, view.size()))
 
-def getIncludesFromContent(contents):
-    return re.compile('\#include *\"(.*)\"').findall(contents)
+def getIncludesFromContent(file_contents):
+    """
+    return all files that is mettioned in #include "file.c"
+    """
+    return re.compile('# *include *\"(.*)\"').findall(file_contents)
+
+def getFunctionsFromContent(file_contents):
+    """
+    return all functions like (type, func_name, parameters)
+    """
+    return re.compile('(# *define|\w+)\** +(?:\w+ +)*\** *([\w]+) *\(([^\)]*)\)\s*(?:;|\{)').findall(file_contents);
+
+def getImportantWordsFromContent(file_contents):
+    """
+    return important words
+    """
+    #adicionando palavras, tipo #define word # adding words like #define word
+    result = re.compile('\#define +(\w+)').findall(file_contents)
+    #adicionando palavras do tipo typedef word snippet; #adding words like typedef word
+    result += re.compile('typedef(?: \w+)+ (\w+);').findall(file_contents)
+    #adicionando palavras do tipo typedef struct {} word; #adding words like typedef struct {} word;
+    result += re.compile('typedef [^\}]*\} ?(\w+);').findall(file_contents)
+    return result
